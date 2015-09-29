a6da5b5cd984a3b5e27e1167e2c6844f3cb0d7c3
Alfredo Deza <alfredo@deza.pe>
2015-09-29 13:33:03 -0400

    we are now a naked ping tool
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 version = '0.0.1'
 
-setup(name="helga-ping",
+setup(name="helga-naked-ping",
       version=version,
       description=('annoy users who insist (or have no idea) on naked pings'),
       classifiers=['Development Status :: 1 - Beta',
@@ -16,12 +16,12 @@ setup(name="helga-ping",
       keywords='irc bot ping',
       author='alfredo deza',
       author_email='alfredo [at] deza [dot] pe',
-      url='https://github.com/alfredodeza/helga-ping',
+      url='https://github.com/alfredodeza/helga-naked-ping',
       license='MIT',
       packages=find_packages(),
       entry_points = dict(
           helga_plugins = [
-              'ping = ping:ping',
+              'naked_ping = naked_ping:naked_ping',
           ],
       ),
 )
