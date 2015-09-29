0f48b05a3d80c105acfa193266480b6f858017ad
Alfredo Deza <alfredo@deza.pe>
2015-09-29 13:37:55 -0400

    add a debug message for matching naked pings
    Signed-off-by: Alfredo Deza <alfredo@deza.pe>

--- naked_ping.py
+++ naked_ping.py
@@ -1,4 +1,7 @@
 from helga.plugins import match
+from helga import log
+
+logger = log.getLogger(__name__)
 
 
 @match(r'\w* ?[:,] ?ping\W*$', priority=101)
@@ -6,4 +9,5 @@ def naked_ping(client, channel, nick, message, matches):
     """
     Annoy users on naked pings
     """
+    logger.debug('matched a naked ping')
     return "%s: consider using pings with data please. See: https://blogs.gnome.org/markmc/2014/02/20/naked-pings/" % nick
